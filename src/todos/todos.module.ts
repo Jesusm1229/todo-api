ac704e500483156ff153258fe5925f85902630e2
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-02-18 15:30:57 -0400

    Adding Validation Pipes

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TodosController } from './todos.controller';
 import { Todos } from './todos.entity';
 import { TodosService } from './todos.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todos])],
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateTodoDto } from './dtos/create-todo.dto';
 import { Todos } from './todos.entity';
 
 @Injectable()
